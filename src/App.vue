9fefc70eb8056a488a3887f801869cb8cc48c7fa
Alfred Beckman <alfred.beckman@prorenata.se>
2024-01-14 22:30:31 +0100

    Change to vuefire to access firestore

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,72 @@
 <template>
-    <h1>Headtracker</h1>
+  <h1>Headtracker</h1>
 
-    <form 
-        @submit.prevent="addTodo"
-    >
-        <input 
-            type="text" 
-            v-model="newTodoContent"
-            >
-        <button>Add info</button>
-    </form>
+  <form @submit.prevent="addTodo">
+    <input
+      type="text"
+      v-model="newTodoContent"
+    />
+    <button>Add info</button>
+  </form>
 
-    <div
-        v-for="todo in todos"
-        :key="todo.id"
-        class="entry"
-    >
-        {{todo.date}} - {{todo.content}} - {{ todo.done?'done':'not done' }}
-        <button
-            @click="deleteTodo(todo.id)"
-        >Delete</button>
-        <button
-            @click="toggleDone(todo.id)"
-        >Done?</button>
-    </div>
+  <div
+    v-for="todo in todos"
+    :key="todo.id"
+    class="entry"
+  >
+    {{ todo.date }} - {{ todo.content }} - {{ todo.done ? 'done' : 'not done' }}
+    <button @click="deleteTodo(todo.id)">Delete</button>
+    <button @click="toggleDone(todo.id)">Done?</button>
+  </div>
 </template>
 
 <script setup>
 /* Imports */
 import { ref, onMounted } from 'vue'
-import { 
-    collection, onSnapshot, 
-    doc, addDoc, deleteDoc, updateDoc, 
-    query, orderBy,  
-}  from "firebase/firestore"
-import { db } from '@/firebase'
+import {
+  collection,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy,
+} from 'firebase/firestore'
+import { todosRef } from '@/firebase'
+import { useCollection } from 'vuefire'
 
 /* Firebase Refs */
+const todosCollectionQuery = query(todosRef, orderBy('date', 'desc'))
 
-const todosCollectionRef = collection(db, "todos") 
-const todosCollectionQuery = query(todosCollectionRef, orderBy("date", 'desc'));
-
-/* Todos */
-const todos = ref([
-/*    {
-        id: 'id1',
-        content: 'Huvudvärk',
-        date: '2024-01-04'
-    },
-    {
-        id: 'id2',
-        content: 'Huvudvärk',
-        date: '2024-01-01'
-    },
-    {
-        id: 'id3',
-        content: 'Huvudvärk',
-        date: '2023-12-25'
-    }*/
-])
-
-/* Get todos */
-
-onMounted(() => {
-    onSnapshot(todosCollectionQuery, (querySnapshot) => {
-        const fbTodos = [];
-        querySnapshot.forEach((doc) => {
-            const todo = {
-                id: doc.id,
-                content: doc.data().content,
-                done: doc.data().done
-            }
-            fbTodos.push(todo)
-        });
-        todos.value = fbTodos
-    });
-})
+const todos = useCollection(todosCollectionQuery)
 
 /* Add todos */
 const newTodoContent = ref('')
 const addTodo = () => {
-    addDoc(todosCollectionRef, {
-        content: newTodoContent.value,
-        date: Date.now(),
-        done: false
-    });
-    newTodoContent.value = ''
+  addDoc(todosRef, {
+    content: newTodoContent.value,
+    date: Date.now(),
+    done: false,
+  })
+  newTodoContent.value = ''
 }
 
 /* Delete todo */
-const deleteTodo = id => {
-    deleteDoc(doc(todosCollectionRef, id))
+const deleteTodo = (id) => {
+  deleteDoc(doc(todosRef, id))
 }
 
 /* Mark Done */
-const toggleDone = id => {
-    const index = todos.value.findIndex(todo => todo.id === id)
-    updateDoc(doc(todosCollectionRef, id), {
-        done: !todos.value[index].done
-    });
+const toggleDone = (id) => {
+  const index = todos.value.findIndex((todo) => todo.id === id)
+  updateDoc(doc(todosRef, id), {
+    done: !todos.value[index].done,
+  })
 }
-
 </script>
 
 <style>
 .entry {
-    margin: 15px 0;
+  margin: 15px 0;
 }
 </style>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7ENwDM7FlarXz1jCAnulT91Mqbf39-UE",
-  authDomain: "head-tracker-413e9.firebaseapp.com",
-  projectId: "head-tracker-413e9",
-  storageBucket: "head-tracker-413e9.appspot.com",
-  messagingSenderId: "14683859563",
-  appId: "1:14683859563:web:a061d65c015fbe111edf2e"
+  apiKey: 'AIzaSyD7ENwDM7FlarXz1jCAnulT91Mqbf39-UE',
+  authDomain: 'head-tracker-413e9.firebaseapp.com',
+  projectId: 'head-tracker-413e9',
+  storageBucket: 'head-tracker-413e9.appspot.com',
+  messagingSenderId: '14683859563',
+  appId: '1:14683859563:web:a061d65c015fbe111edf2e',
 }
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp)
 
-export {
-    db
-}
+const todosRef = collection(db, 'todos')
+
+export { firebaseApp, db, todosRef }
